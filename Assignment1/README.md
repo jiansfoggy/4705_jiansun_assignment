bdb7c29d4c0233af48b69be4471f6ba842f0d4d2
jiansfogg <Jian.Sun86@du.edu>
2025-06-25 23:39:00 -0600

    add readme file

---
+++ Assignment1/README.md
@@ -0,0 +1,9 @@
+# How to run this app
+
+- Download the entire folder by `git clone git@github.com:jiansfoggy/4705_jiansun_assignment.git`
+
+- Enter the folder `cd Assignment1`
+
+- Data Preparation and Model Training `python3 train_model.py`
+
+- Building the Streamlit Application `streamlit run app.py`
--- Assignment1/train_model.py
+++ Assignment1/train_model.py
@@ -32,3 +32,14 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+# How to run this app
+
+- Download the entire folder by `git clone git@github.com:jiansfoggy/4705_jiansun_assignment.git`
+
+- Enter the folder `cd Assignment1`
+  	```
+
+- Data Preparation and Model Training `python3 train_model.py`
+
+- Building the Streamlit Application `streamlit run app.py`
